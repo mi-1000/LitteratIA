cf3bf218bc7d63334cff12b6853981d9a477c53a
mi-1000 <119438442+mi-1000@users.noreply.github.com>
2026-04-20 23:50:51 +0200

    fixed bug in production when switching languages redirects to comparia.beta.gouv.fr

--- frontend/src/lib/global.svelte.ts
+++ frontend/src/lib/global.svelte.ts
@@ -9,7 +9,7 @@ const disabledLocaleCodes = ["da", "lt", "sv"] //env.PUBLIC_DISABLED_LOCALES
 
 export type LocaleOption = { code: Locale; short: string; long: string; host: string }
 
-const DEFAULT_HOST = dev ? 'localhost:5173' : 'comparia.beta.gouv.fr'
+const DEFAULT_HOST = dev ? 'localhost:5173' : 'litteratia.atilf.fr'
 export const HOST_TO_LOCALE = dev
   ? {
       '127.0.0.1:8080': 'da'
